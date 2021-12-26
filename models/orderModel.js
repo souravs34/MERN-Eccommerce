82b9dc704e0979276359227cbfd6e324b0df793b
Sourav34 <borateourav@gmail.com>
2021-12-26 19:19:29 +0530

    Order Model Problem SOlved

--- models/orderModel.js
+++ models/orderModel.js
@@ -12,7 +12,7 @@ const orderSchema = mongoose.Schema(
         qty: { type: Number, required: true },
         image: { type: String, required: true },
         price: { type: String, required: true },
-        products: {
+        product: {
           type: mongoose.Schema.Types.ObjectId,
           required: true,
           ref: "Product",
@@ -22,7 +22,7 @@ const orderSchema = mongoose.Schema(
     shippingAddress: {
       address: { type: String, required: true },
       city: { type: String, required: true },
-      poatalCode: { type: String, required: true },
+      postalCode: { type: String, required: true },
       country: { type: String, required: true },
     },
     paymentMethod: {
