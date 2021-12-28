c31fb6d484f2757c8557c6ad59af5d2867139e31
Sourav34 <borateourav@gmail.com>
2021-12-28 11:08:42 +0530

    Done productListScreen and delete Api

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import PlaceOrder from "./Pages/PlaceOrder";
 import OrderScreen from "./Pages/OrderScreen";
 import UserListScreen from "./Pages/UserListScreen";
 import UserEditScreen from "./Pages/UserEditScreen";
+import ProductListScreen from "./Pages/ProductListScreen";
 
 const App = () => {
   return (
@@ -47,6 +48,9 @@ const App = () => {
           </Route>
           <Route path="/admin/user/:id/edit">
             <UserEditScreen />
+          </Route>
+          <Route path="/admin/productlist">
+            <ProductListScreen />
           </Route>
           <Route path="/product/:id">
             <ProductPage />
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -5,9 +5,10 @@ const router = express.Router();
 import {
   getProducts,
   getProductById,
+  deleteProduct,
 } from "../controllers/productController.js";
-
+import { protect, admin } from "../middleware/authMiddleware.js";
 router.get("/", getProducts);
 
-router.get("/:id", getProductById);
+router.route("/:id").get(getProductById).delete(protect, admin, deleteProduct);
 export default router;
