e81b8f5a2c9fe3d05e606d57bfb302b78e72e8e3
Sourav34 <borateourav@gmail.com>
2021-12-29 10:45:56 +0530

    Multer route backend setup

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 import dotenv from "dotenv";
 import express from "express";
+import path from "path";
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
+import uploadRoutes from "./routes/uploadRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 dotenv.config();
 connectDB();
@@ -17,7 +19,11 @@ app.get("/", (req, res) => {
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
+app.use("/api/uploads", uploadRoutes);
 
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+//Upload making folder static
 app.get("/api/config/paypal", (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 );
