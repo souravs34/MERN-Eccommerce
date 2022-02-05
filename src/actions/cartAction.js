238af97342499a67010332b00f49f80c28fa6ea1
Sourav34 <borateourav@gmail.com>
2022-02-05 19:43:29 +0530

    Deploy2

--- src/actions/cartAction.js
+++ src/actions/cartAction.js
@@ -9,7 +9,9 @@ import {
 
 //getState => get all things from store reducer
 export const addToCart = (id, qty) => async (dispatch, getState) => {
-  const { data } = await axios.get(`/api/products/${id}`);
+  const { data } = await axios.get(
+    `${process.env.REACT_APP_URL}/api/products/${id}`
+  );
 
   dispatch({
     type: CART_ADD_ITEM,
